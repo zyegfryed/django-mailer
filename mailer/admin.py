47c1425201a4e58727a8ebf924a93d8316ba9a44
Dan McGee <dpmcgee@gmail.com>
2010-10-01 14:17:16 -0500

    Make admin a bit more functional
    Signed-off-by: Dan McGee <dpmcgee@gmail.com>

--- mailer/admin.py
+++ mailer/admin.py
@@ -5,14 +5,19 @@ from mailer.models import Message, DontSendEntry, MessageLog
 
 class MessageAdmin(admin.ModelAdmin):
     list_display = ["id", "to_addresses", "subject", "when_added", "priority"]
+    list_filter = ["priority", "when_added"]
+    date_hierarchy = "when_added"
 
 
 class DontSendEntryAdmin(admin.ModelAdmin):
     list_display = ["to_address", "when_added"]
+    search_fields = ["to_address"]
 
 
 class MessageLogAdmin(admin.ModelAdmin):
-    list_display = ["id", "to_addresses", "subject", "when_attempted", "result"]
+    list_display = ["id", "to_addresses", "subject", "when_added", "when_attempted", "priority", "result"]
+    list_filter = ["result", "priority", "when_added", "when_attempted"]
+    date_hierarchy = "when_attempted"
 
 
 admin.site.register(Message, MessageAdmin)
